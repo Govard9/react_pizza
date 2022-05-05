9e06079bf0726ed0db9716a8edaf64286960b38a
govard9 <web-supsu@yandex.ru>
2022-05-05 17:45:54 +0700

    4 lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
-import logoSvg from '../assets/img/pizza-logo.svg'
-import { Button } from './Button'
+import logoSvg from "../assets/img/pizza-logo.svg";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 export const Header = (props) => {
-    return (
-        <div className="header">
-        <div className="container">
-          <div className="header__logo">
-            <img width="38" src={logoSvg} alt="Pizza logo" />
-            <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
-            </div>
-          </div>
-          <Button onClick={ props.onClick } />
-        </div>
-      </div>
-    )
-} 
+	return (
+		<div className="header">
+			<div className="container">
+      <Link to={"/"}>
+      <div className="header__logo">
+					<img width="38" src={logoSvg} alt="Pizza logo" />
+					<div>
+						<h1>React Pizza</h1>
+						<p>самая вкусная пицца во вселенной</p>
+					</div>
+				</div>
+      </Link>
+				<div className="header__cart">
+					<Link to={"/cart"}>
+						<Button />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './scss/app.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/app.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
